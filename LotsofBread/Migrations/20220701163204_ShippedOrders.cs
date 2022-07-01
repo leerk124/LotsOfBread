f7e71f963899c3e972c0ce463afd49e4ea9d7f96
Robin Lee <n00835112@unf.edu>
2022-07-01 12:34:26 -0400

    FIXED DB FOR CREDIT INFO - CART

---
+++ LotsofBread/Migrations/20220701163204_ShippedOrders.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace LotsofBread.Migrations
+{
+    public partial class ShippedOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "CVV",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "CardNumber",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExpMonth",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ExpYear",
+                table: "Orders",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NameOnCard",
+                table: "Orders",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CVV",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "CardNumber",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ExpMonth",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "ExpYear",
+                table: "Orders");
+
+            migrationBuilder.DropColumn(
+                name: "NameOnCard",
+                table: "Orders");
+        }
+    }
+}
